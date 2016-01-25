5d2c043a5fe1616a89d8fe531079e9c3fa74e711
Jacob Konshuk <Jacob Konshuk>
2016-01-25 08:55:37 -0700

    This is a Hello world program!

---
+++ JavaPractice.java
@@ -0,0 +1,16 @@
+/*************************
+ * @Author Jacob Konshuk
+ *************************/
+
+public class JavaPractice {
+    /******************************************
+     * This program reads in a file and prints
+     * each line preceeded by the number of words
+     * in that line.
+     * @param args
+     ******************************************/
+    public static void main(String[] args) {
+        //header
+        System.out.println("Hello World");
+    }      
+}

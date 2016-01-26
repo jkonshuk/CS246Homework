e22ef00b27a6f447a8c0af4bffc10448593aa966
Jacob Konshuk <Jacob Konshuk>
2016-01-26 09:50:10 -0700

    this has the correct filename

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/*************************
+ * @Author Jacob Konshuk
+ *************************/
+
+public class HelloWorld {
+    /******************************************
+     * This program reads in a file and prints
+     * each line preceeded by the number of words
+     * in that line.
+     * @param args
+     ******************************************/
+    public static void main(String[] args) {
+        //header
+        System.out.println("Hello GitHub World");
+    }      
+}
